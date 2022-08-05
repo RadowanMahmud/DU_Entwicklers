e5e2e663f75639f54eb9360fd1629f55974cd110
Ashraful Gafur <ashraful.gafur@streamstech.com>
2022-08-05 07:30:43 +0600

    speech to text done

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,65 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {HttpHeaders} from '@angular/common/http';
+
 declare var $: any;
 import * as RecordRTC from 'recordrtc';
-import { DomSanitizer } from '@angular/platform-browser';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  title = 'micRecorder';
-  //Lets declare Record OBJ
-  record;
-  //Will use this flag for toggeling recording
-  recording = false;
-  //URL of Blob
-  url;
-  error;
+    title = 'micRecorder';
+    // Lets declare Record OBJ
+    record;
+    // Will use this flag for toggeling recording
+    recording = false;
+    // URL of Blob
+    url;
+    error;
+    audioFile: File;
+    audioInText: '';
 
-  constructor(private domSanitizer: DomSanitizer) {}
+    constructor(private domSanitizer: DomSanitizer, private http: HttpClient) {
+    }
 
-  sanitize(url: string) {
-    return this.domSanitizer.bypassSecurityTrustUrl(url);
-  }
+    sanitize(url: string) {
+        return this.domSanitizer.bypassSecurityTrustUrl(url);
+    }
 
-  startRecord() {
-    this.recording = true;
-    let mediaConstraints = {
-      video: false,
-      audio: true
-    };
-    navigator.mediaDevices.getUserMedia(mediaConstraints).then(this.successCallback.bind(this), this.errorCallback.bind(this));
-  }
-  
-  successCallback(stream) {
-    var options = {
-      mimeType: "audio/wav",
-      numberOfAudioChannels: 1,
-      sampleRate: 16000,
-    };
-    //Start Actuall Recording
-    var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
-    this.record = new StereoAudioRecorder(stream, options);
-    this.record.record();
-  }
+    startRecord() {
+        this.recording = true;
+        const mediaConstraints = {
+            video: false,
+            audio: true
+        };
+        navigator.mediaDevices.getUserMedia(mediaConstraints).then(this.successCallback.bind(this), this.errorCallback.bind(this));
+    }
 
-  stopRecord() {
-    this.recording = false;
-    this.record.stop(this.processRecording.bind(this));
-  }
- 
-  processRecording(blob) {
-    this.url = URL.createObjectURL(blob);
-    console.log("blob", blob);
-    console.log("url", this.url);
-  }
+    successCallback(stream) {
+        const options = {
+            mimeType: 'audio/wav',
+            numberOfAudioChannels: 1,
+            sampleRate: 44100,
+        };
+        // Start Actuall Recording
+        const StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
+        this.record = new StereoAudioRecorder(stream, options);
+        this.record.record();
+    }
 
-  errorCallback(error) {
-    this.error = 'Can not play audio in your browser';
-  }
-  ngOnInit() {}
+    stopRecord() {
+        this.recording = false;
+        this.record.stop(this.processRecording.bind(this));
+    }
+
+    processRecording(blob) {
+        this.url = URL.createObjectURL(blob);
+        console.log('blob', blob);
+        console.log('url', this.url);
+        this.audioFile = new File([blob], 'audioFile.wav');
+        this.makeStructureForApi();
+    }
+
+    makeStructureForApi() {
+        const headers = new HttpHeaders()
+            .set('content-type', 'audio/wav')
+            .set('Accept', '*/*')
+            .set('Ocp-Apim-Subscription-Key', '640c875c440440abb2947b30bbd36940')
+            .set('Access-Control-Allow-Headers', 'Content-Type')
+            .set('Access-Control-Allow-Origin', '*');
+
+        const azure_url = 'https://southeastasia.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US';
+        this.http.post(azure_url, this.audioFile, {'headers': headers}).subscribe((res: any) => {
+            this.audioInText = res.DisplayText;
+            console.log(this.audioInText);
+        });
+    }
+
+    errorCallback(error) {
+        this.error = 'Can not play audio in your browser';
+    }
+
+    ngOnInit() {
+    }
 
 }
--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -1,96 +1,106 @@
-import { Injectable, NgZone } from '@angular/core';
-import { User } from '../services/user';
+import {Injectable, NgZone} from '@angular/core';
+import {User} from '../services/user';
 import * as auth from 'firebase/auth';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {AngularFireAuth} from '@angular/fire/compat/auth';
+import {HttpClient} from '@angular/common/http';
 import {
-  AngularFirestore,
-  AngularFirestoreDocument,
+    AngularFirestore,
+    AngularFirestoreDocument,
 } from '@angular/fire/compat/firestore';
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
+
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class AuthService {
-  userData: any; // Save logged in user data
-  constructor(
-    public afs: AngularFirestore, // Inject Firestore service
-    public afAuth: AngularFireAuth, // Inject Firebase auth service
-    public router: Router,
-    public ngZone: NgZone // NgZone service to remove outside scope warning
-  ) {
-    /* Saving user data in localstorage when 
-    logged in and setting up null when logged out */
-    this.afAuth.authState.subscribe((user) => {
-      if (user) {
-        this.userData = user;
-        localStorage.setItem('isLoggedIn', JSON.stringify(this.userData));
-        JSON.parse(localStorage.getItem('isLoggedIn')!);
-      } else {
-        localStorage.setItem('isLoggedIn', 'null');
-        JSON.parse(localStorage.getItem('isLoggedIn')!);
-      }
-    });
-  }
-  // Sign in with email/password
-  SignIn(email: string, password: string) {
-    return this.afAuth
-      .signInWithEmailAndPassword(email, password)
-      .then((result) => {
-        this.SetUserData(result.user);
-        this.router.navigateByUrl('dashboard');
-      })
-      .catch((error) => {
-        window.alert(error.message);
-      });
-  }
+    userData: any; // Save logged in user data
+    constructor(
+        private http: HttpClient,
+        public afs: AngularFirestore, // Inject Firestore service
+        public afAuth: AngularFireAuth, // Inject Firebase auth service
+        public router: Router,
+        public ngZone: NgZone // NgZone service to remove outside scope warning
+    ) {
+        /* Saving user data in localstorage when
+        logged in and setting up null  when logged out */
+        this.afAuth.authState.subscribe((user) => {
+            if (user) {
+                this.userData = user;
+                localStorage.setItem('isLoggedIn', JSON.stringify(this.userData));
+                JSON.parse(localStorage.getItem('isLoggedIn')!);
+            } else {
+                localStorage.setItem('isLoggedIn', 'null');
+                JSON.parse(localStorage.getItem('isLoggedIn')!);
+            }
+        });
+    }
 
-  get isLoggedIn(): boolean {
-    const user = JSON.parse(localStorage.getItem('isLoggedIn')!);
-    return user !== null && user.emailVerified !== false ? true : false;
-  }
-  // Sign in with Google
-  GoogleAuth() {
-    return this.AuthLogin(new auth.GoogleAuthProvider()).then((res: any) => {
-      if (res) {
-        this.router.navigateByUrl('dashboard');
-      }
-    });
-  }
-  // Auth logic to run auth providers
-  AuthLogin(provider: any) {
-    return this.afAuth
-      .signInWithPopup(provider)
-      .then((result) => {
-        this.SetUserData(result.user);
-        this.router.navigateByUrl('dashboard');
-      })
-      .catch((error) => {
-        window.alert(error);
-      });
-  }
-  /* Setting up user data when sign in with username/password, 
-  sign up with username/password and sign in with social auth  
-  provider in Firestore database using AngularFirestore + AngularFirestoreDocument service */
-  SetUserData(user: any) {
-    const userRef: AngularFirestoreDocument<any> = this.afs.doc(
-      `users/${user.uid}`
-    );
-    const userData: User = {
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      emailVerified: user.emailVerified,
-    };
-    return userRef.set(userData, {
-      merge: true,
-    });
-  }
-  // Sign out
-  SignOut() {
-    return this.afAuth.signOut().then(() => {
-      localStorage.removeItem('isLoggedIn');
-      this.router.navigateByUrl('login');
-    });
-  }
+
+    // Sign in with email/password
+    SignIn(email: string, password: string) {
+        return this.afAuth
+            .signInWithEmailAndPassword(email, password)
+            .then((result) => {
+                this.SetUserData(result.user);
+                this.router.navigateByUrl('dashboard');
+            })
+            .catch((error) => {
+                window.alert(error.message);
+            });
+    }
+
+    get isLoggedIn(): boolean {
+        const user = JSON.parse(localStorage.getItem('isLoggedIn')!);
+        return user !== null && user.emailVerified !== false ? true : false;
+    }
+
+    // Sign in with Google
+    GoogleAuth() {
+        return this.AuthLogin(new auth.GoogleAuthProvider()).then((res: any) => {
+            if (res) {
+                this.router.navigateByUrl('dashboard');
+            }
+        });
+    }
+
+    // Auth logic to run auth providers
+    AuthLogin(provider: any) {
+        return this.afAuth
+            .signInWithPopup(provider)
+            .then((result) => {
+                this.SetUserData(result.user);
+                this.router.navigateByUrl('dashboard');
+            })
+            .catch((error) => {
+                window.alert(error);
+            });
+    }
+
+    /* Setting up user data when sign in with username/password,
+    sign up with username/password and sign in with social auth
+    provider in Firestore database using AngularFirestore + AngularFirestoreDocument service */
+    SetUserData(user: any) {
+        const userRef: AngularFirestoreDocument<any> = this.afs.doc(
+            `users/${user.uid}`
+        );
+        const userData: User = {
+            uid: user.uid,
+            email: user.email,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            emailVerified: user.emailVerified,
+        };
+        return userRef.set(userData, {
+            merge: true,
+        });
+    }
+
+    // Sign out
+    SignOut() {
+        return this.afAuth.signOut().then(() => {
+            localStorage.removeItem('isLoggedIn');
+            this.router.navigateByUrl('login');
+        });
+    }
 }
