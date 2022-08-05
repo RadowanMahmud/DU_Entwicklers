f735da8739b1000c6e80b1bd4090fc5fb3c5e1b0
Jahid1999 <bsse1030@iit.du.ac.bd>
2022-08-05 07:25:50 +0600

    added voice capture for notes

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+declare var $: any;
+import * as RecordRTC from "recordrtc";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-table-list',
-  templateUrl: './table-list.component.html',
-  styleUrls: ['./table-list.component.css']
+  selector: "app-table-list",
+  templateUrl: "./table-list.component.html",
+  styleUrls: ["./table-list.component.css"],
 })
 export class TableListComponent implements OnInit {
+  title = "micRecorder";
+  //Lets declare Record OBJ
+  record;
+  //Will use this flag for toggeling recording
+  recording = false;
+  //URL of Blob
+  url;
+  error;
 
-  constructor() { }
+  constructor(private domSanitizer: DomSanitizer) {}
 
-  ngOnInit() {
+  sanitize(url: string) {
+    return this.domSanitizer.bypassSecurityTrustUrl(url);
   }
 
+  startRecord() {
+    this.recording = true;
+    let mediaConstraints = {
+      video: false,
+      audio: true,
+    };
+    navigator.mediaDevices
+      .getUserMedia(mediaConstraints)
+      .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+  }
+
+  successCallback(stream) {
+    var options = {
+      mimeType: "audio/wav",
+      numberOfAudioChannels: 1,
+      sampleRate: 44100,
+    };
+    //Start Actuall Recording
+    var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
+    this.record = new StereoAudioRecorder(stream, options);
+    this.record.record();
+  }
+
+  stopRecord() {
+    this.recording = false;
+    this.record.stop(this.processRecording.bind(this));
+  }
+
+  processRecording(blob) {
+    this.url = URL.createObjectURL(blob);
+    console.log("blob", blob);
+    console.log("url", this.url);
+  }
+
+  errorCallback(error) {
+    this.error = "Can not play audio in your browser";
+  }
+
+  ngOnInit() {}
 }
