1b4cdc128d8de9976df5024b9651d65ad067cc78
RadowanMahmud <bsse1002@iit.du.ac.bd>
2022-08-05 06:51:07 +0600

    added map

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ViewChild} from '@angular/core'
 declare const google: any;
+declare const Microsoft: any;
 
 interface Marker {
 lat: number;
@@ -15,111 +16,20 @@ draggable?: boolean;
 })
 export class MapsComponent implements OnInit {
 
-  constructor() { }
+    @ViewChild('map') myMap; // using ViewChild to reference the div instead of setting an id
+    public pageTitle: string = "Jar Assistant Map";
+  
+    ngAfterViewInit(){  // after the view completes initializaion, create the map
+      var map = new Microsoft.Maps.Map("#map", {
+          credentials: 'AltZf2Vz8P_Cg9vl1WuLK5prxtmThEinpoQUGf3kzxaVArsHfCwcXBCENsak74BX'
+      });
+      var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), null);
+      var layer = new Microsoft.Maps.Layer();
+      layer.add(pushpin);
+      map.layers.insert(layer);
+    }
 
   ngOnInit() {
-
-    var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
-    var mapOptions = {
-        zoom: 13,
-        center: myLatlng,
-        scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-        styles: [{
-            "featureType": "water",
-            "stylers": [{
-                "saturation": 43
-            }, {
-                "lightness": -11
-            }, {
-                "hue": "#0088ff"
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "hue": "#ff0000"
-            }, {
-                "saturation": -100
-            }, {
-                "lightness": 99
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#808080"
-            }, {
-                "lightness": 54
-            }]
-        }, {
-            "featureType": "landscape.man_made",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#ece2d9"
-            }]
-        }, {
-            "featureType": "poi.park",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#ccdca1"
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "labels.text.fill",
-            "stylers": [{
-                "color": "#767676"
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "labels.text.stroke",
-            "stylers": [{
-                "color": "#ffffff"
-            }]
-        }, {
-            "featureType": "poi",
-            "stylers": [{
-                "visibility": "off"
-            }]
-        }, {
-            "featureType": "landscape.natural",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "visibility": "on"
-            }, {
-                "color": "#b8cb93"
-            }]
-        }, {
-            "featureType": "poi.park",
-            "stylers": [{
-                "visibility": "on"
-            }]
-        }, {
-            "featureType": "poi.sports_complex",
-            "stylers": [{
-                "visibility": "on"
-            }]
-        }, {
-            "featureType": "poi.medical",
-            "stylers": [{
-                "visibility": "on"
-            }]
-        }, {
-            "featureType": "poi.business",
-            "stylers": [{
-                "visibility": "simplified"
-            }]
-        }]
-
-    };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        title: "Hello World!"
-    });
-
-    // To add the marker to the map, call setMap();
-    marker.setMap(map);
-  }
+    }
 
 }
